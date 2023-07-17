cf1df25e95ca0e5d78e3a4fafb6949c86b668183
unknown <roushanraj395@gmail.com>
2023-07-17 07:20:14 +0530

    tradational approach of adding properties

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Counter from './components/counter/Counter';
 
+//passing properties in components
 function App() {
   return (
     <div className="App">
+      <PlayingWithProps property1="value1" property2="value2"></PlayingWithProps>
+      <Counter></Counter>
+      <Counter></Counter>
       <Counter></Counter>
     </div>
   );
+}
+
+function PlayingWithProps(properties) {
+  //tradational approach of passing properties
+  console.log(properties)
+  console.log(properties.property1)
+  console.log(properties.property2)
+  return (
+    <div>props</div>
+  )
 }
 
 export default App;
